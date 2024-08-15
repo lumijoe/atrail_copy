2337edfed7cccd1c5014576e3936c6216fc554e1
lumijoe <spacedesign63@gmail.com>
2024-08-15 22:16:29 +0900

    test

--- content-contribution.php
+++ content-contribution.php
@@ -1,12 +1,9 @@
                     <article class="article-card">
-                      <a class="card-link" href="<?php echo get_term_link( $term ); ?>">
+                      <a class="card-link" href="<?php echo get_term_link($term); ?>">
                         <div class="image">
-<?php
-$image_id = get_field( 'event_image', $term->taxonomy. '_'. $term->term_id );
-echo wp_get_attachment_image( $image_id, 'contribution' );
-?>
-						</div>
-                        <div class="body">
+                          <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-page-dummy.png" />
+                        </div>
+                        <div class=" body">
                           <p class="title"><?php echo $term->name; ?></p>
                           <p class="excerpt"><?php echo $term->description; ?></p>
                           <div class="buttonBox">
